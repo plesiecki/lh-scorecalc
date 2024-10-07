16845fccf2e331c39e91c862c709567d1aa816d4
Paweł Lesiecki <pawel.lesiecki@allegro.com>
2024-10-07 13:07:58 +0200

    scorecalc-update

---
+++ script/gauge.js
@@ -0,0 +1,282 @@
+'use strict';
+
+/* Most of the impressive code here authored by Ana Tudor, Nov 2019 */
+
+import {calculateRating} from './util.js';
+
+const delay = delay => new Promise(resolve => setTimeout(resolve, delay));
+
+export function updateGauge(wrapper, category) {
+  _setPerfGaugeExplodey(wrapper, category);
+}
+
+function _determineTrig(sizeSVG, percent, strokeWidth) {
+  strokeWidth = strokeWidth || sizeSVG / 32;
+
+  const radiusInner = sizeSVG / strokeWidth;
+  const strokeGap = 0.5 * strokeWidth;
+  const radiusOuter = radiusInner + strokeGap + strokeWidth;
+
+  const circumferenceInner = 2 * Math.PI * radiusInner;
+  // arc length we need to subtract
+  // for very small strokeWidth:radius ratios this is ≈ strokeWidth
+  // angle = acute angle of isosceles △ with 2 edges equal to radius & 3rd equal to strokeWidth
+  // angle formula given by law of cosines
+  const endDiffInner = Math.acos(1 - 0.5 * Math.pow((0.5 * strokeWidth) / radiusInner, 2)) * radiusInner;
+
+  const circumferenceOuter = 2 * Math.PI * radiusOuter;
+  const endDiffOuter = Math.acos(1 - 0.5 * Math.pow((0.5 * strokeWidth) / radiusOuter, 2)) * radiusOuter;
+
+  return {
+    radiusInner,
+    radiusOuter,
+    circumferenceInner,
+    circumferenceOuter,
+    getArcLength: _ => Math.max(0, +(percent * circumferenceInner - 2 * endDiffInner).toFixed(4)),
+    // isButt case is for metricArcHoverTarget
+    getMetricArcLength: (weightingPct, isButt) => {
+      // TODO: this math isn't perfect butt it's very close.
+      const linecapFactor = isButt ? 0 : 2 * endDiffOuter;
+      return Math.max(0, +(weightingPct * circumferenceOuter - strokeGap - linecapFactor).toFixed(4));
+    },
+    endDiffInner,
+    endDiffOuter,
+    strokeWidth,
+    strokeGap,
+  };
+}
+
+/**
+ * @param {HTMLAnchorElement} wrapper
+ * @param {LH.ReportResult.Category} category
+ */
+function _setPerfGaugeExplodey(wrapper, category) {
+  const sizeSVG = 128;
+  const offsetSVG = -0.5 * sizeSVG;
+
+  const percent = Number(category.score);
+  const {
+    radiusInner,
+    radiusOuter,
+    circumferenceInner,
+    circumferenceOuter,
+    getArcLength,
+    getMetricArcLength,
+    endDiffInner,
+    endDiffOuter,
+    strokeWidth,
+    strokeGap,
+  } = _determineTrig(sizeSVG, percent);
+
+  const SVG = wrapper.querySelector('.lh-gauge');
+  const NS_URI = 'http://www.w3.org/2000/svg';
+
+  SVG.setAttribute('viewBox', [offsetSVG, offsetSVG, sizeSVG, sizeSVG].join(' '));
+  SVG.style.setProperty('--stroke-width', `${strokeWidth}px`);
+  SVG.style.setProperty('--circle-meas', 2 * Math.PI.toFixed(4));
+
+  const groupOuter = wrapper.querySelector('.lh-gauge__outer');
+  const groupInner = wrapper.querySelector('.lh-gauge__inner');
+  const cover = groupOuter.querySelector('.cover');
+  const gaugeArc = groupInner.querySelector('.lh-gauge__arc');
+  const gaugePerc = groupInner.querySelector('.lh-gauge__percentage');
+
+  groupOuter.style.setProperty('--scale-initial', radiusInner / radiusOuter);
+  groupOuter.style.setProperty('--radius', `${radiusOuter}px`);
+  cover.style.setProperty('--radius', `${0.5 * (radiusInner + radiusOuter)}px`);
+  cover.setAttribute('stroke-width', strokeGap);
+  SVG.style.setProperty('--radius', `${radiusInner}px`);
+
+  gaugeArc.setAttribute('stroke-dasharray', `${getArcLength()} ${(circumferenceInner - getArcLength()).toFixed(4)}`);
+  gaugeArc.setAttribute('stroke-dashoffset', 0.25 * circumferenceInner - endDiffInner);
+
+  gaugePerc.textContent = Math.round(percent * 100);
+
+  const radiusTextOuter = radiusOuter + strokeWidth;
+  const radiusTextInner = radiusOuter - strokeWidth;
+
+  const metrics = category.auditRefs.filter(r => r.group === 'metrics' && r.weight);
+  const totalWeight = metrics.reduce((sum, each) => (sum += each.weight), 0);
+  let offsetAdder = 0.25 * circumferenceOuter - endDiffOuter - 0.5 * strokeGap;
+  let angleAdder = -0.5 * Math.PI;
+
+  // Extra hack on top of the HACK for element reuse below. Delete any metric elems that aren't needed anymore (happens when the same gauge goes from v5 to v6)
+  groupOuter.querySelectorAll('.metric').forEach(metricElem => {
+    const classNamesToRetain = metrics.map(metric => `metric--${metric.id}`);
+    const match = classNamesToRetain.find(selector => metricElem.classList.contains(selector));
+    if (!match) metricElem.remove();
+  });
+
+  metrics.forEach((metric, i) => {
+    // TODO(porting to real LHR..): in scorecalc we dont use the real audit ID just the acronym.
+    const alias = metric.id;
+
+    // Hack
+    const needsDomPopulation = !groupOuter.querySelector(`.metric--${alias}`);
+
+    // HACK:This isn't ideal but it was quick. Create element during initialization or reuse existing during updates
+    const metricGroup = groupOuter.querySelector(`.metric--${alias}`) || document.createElementNS(NS_URI, 'g');
+    const metricArcMax = groupOuter.querySelector(`.metric--${alias} .lh-gauge--faded`) || document.createElementNS(NS_URI, 'circle');
+    const metricArc = groupOuter.querySelector(`.metric--${alias} .lh-gauge--miniarc`) || document.createElementNS(NS_URI, 'circle');
+    const metricArcHoverTarget = groupOuter.querySelector(`.metric--${alias} .lh-gauge-hover`) || document.createElementNS(NS_URI, 'circle');
+    const metricLabel = groupOuter.querySelector(`.metric--${alias} .metric__label`) || document.createElementNS(NS_URI, 'text');
+    const metricValue = groupOuter.querySelector(`.metric--${alias} .metric__value`) || document.createElementNS(NS_URI, 'text');
+
+    metricGroup.classList.add('metric', `metric--${alias}`);
+    metricArcMax.classList.add('lh-gauge__arc', 'lh-gauge__arc--metric', 'lh-gauge--faded');
+    metricArc.classList.add('lh-gauge__arc', 'lh-gauge__arc--metric', 'lh-gauge--miniarc');
+    metricArcHoverTarget.classList.add('lh-gauge__arc', 'lh-gauge__arc--metric', 'lh-gauge--faded', 'lh-gauge-hover');
+
+    const weightingPct = metric.weight / totalWeight;
+    const metricLengthMax = getMetricArcLength(weightingPct);
+    const metricPercent = metric.result.score * weightingPct;
+    const metricLength = getMetricArcLength(metricPercent);
+    const metricOffset = weightingPct * circumferenceOuter;
+    const metricHoverLength = getMetricArcLength(weightingPct, true);
+
+    metricGroup.style.setProperty('--metric-color', `var(--palette-${i})`);
+    metricGroup.style.setProperty('--metric-offset', `${offsetAdder}`);
+    metricGroup.style.setProperty('--i', i);
+
+    metricArcMax.setAttribute('stroke-dasharray', `${metricLengthMax} ${circumferenceOuter - metricLengthMax}`);
+    metricArc.style.setProperty('--metric-array', `${metricLength} ${circumferenceOuter - metricLength}`);
+    metricArcHoverTarget.setAttribute('stroke-dasharray', `${metricHoverLength} ${circumferenceOuter - metricHoverLength - endDiffOuter}`);
+
+    metricLabel.classList.add('metric__label');
+    metricValue.classList.add('metric__value');
+    metricLabel.textContent = alias;
+    metricValue.textContent = `+${Math.round(metricPercent * 100)}`;
+
+    const midAngle = angleAdder + weightingPct * Math.PI;
+    const cos = Math.cos(midAngle);
+    const sin = Math.sin(midAngle);
+
+    // only set non-default alignments
+    switch (true) {
+      case cos > 0:
+        metricValue.setAttribute('text-anchor', 'end');
+        break;
+      case cos < 0:
+        metricLabel.setAttribute('text-anchor', 'end');
+        break;
+      case cos === 0:
+        metricLabel.setAttribute('text-anchor', 'middle');
+        metricValue.setAttribute('text-anchor', 'middle');
+        break;
+    }
+
+    switch (true) {
+      case sin > 0:
+        metricLabel.setAttribute('dominant-baseline', 'hanging');
+        break;
+      case sin < 0:
+        metricValue.setAttribute('dominant-baseline', 'hanging');
+        break;
+      case sin === 0:
+        metricLabel.setAttribute('dominant-baseline', 'middle');
+        metricValue.setAttribute('dominant-baseline', 'middle');
+        break;
+    }
+
+    metricLabel.setAttribute('x', (radiusTextOuter * cos).toFixed(2));
+    metricLabel.setAttribute('y', (radiusTextOuter * sin).toFixed(2));
+    metricValue.setAttribute('x', (radiusTextInner * cos).toFixed(2));
+    metricValue.setAttribute('y', (radiusTextInner * sin).toFixed(2));
+
+    if (needsDomPopulation) {
+      metricGroup.appendChild(metricArcMax);
+      metricGroup.appendChild(metricArc);
+      metricGroup.appendChild(metricArcHoverTarget);
+      metricGroup.appendChild(metricLabel);
+      metricGroup.appendChild(metricValue);
+      groupOuter.appendChild(metricGroup);
+    }
+
+    offsetAdder -= metricOffset;
+    angleAdder += weightingPct * 2 * Math.PI;
+  });
+
+  // Hack. Not ideal.
+  if (SVG.dataset.listenersSetup) return;
+  SVG.dataset.listenersSetup = true;
+
+  // peekGauge(SVG);
+
+  /*
+    wrapper.state-expanded: gauge is exploded
+    wrapper.state-highlight: gauge is exploded and one of the metrics is being highlighted
+    metric.metric-highlight: highlight this particular metric
+  */
+  SVG.addEventListener('mouseover', e => {
+    // if hovering on the SVG and its expanded, get rid of everything
+    if (e.target === SVG && wrapper.classList.contains('state--expanded')) {
+      // paul: not sure why we want to remove this.. seems like we want to keep it expanded...
+      SVG.classList.remove('state--expanded');
+
+      if (SVG.classList.contains('state--highlight')) {
+        SVG.classList.remove('state--highlight');
+        SVG.querySelector('.metric--highlight').classList.remove('metric--highlight');
+      }
+      return;
+    }
+
+    const parent = e.target.parentNode;
+
+    // if hovering on a metric, highlight that one.
+    // TODO: The hover target is a little small. ideally it's thicker.
+    if (parent && parent.classList && parent.classList.contains('metric')) {
+      // match the bg color of the gauge during a metric highlight
+      wrapper.style.setProperty('--color-highlight', `var(--palette-${parent.style.getPropertyValue('--i')})`);
+
+      if (!SVG.classList.contains('state--highlight')) {
+        SVG.classList.add('state--highlight');
+        parent.classList.add('metric--highlight');
+      } else {
+        const highlighted = SVG.querySelector('.metric--highlight');
+
+        if (highlighted && parent !== highlighted) {
+          highlighted.classList.remove('metric--highlight');
+          parent.classList.add('metric--highlight');
+        }
+      }
+    }
+  });
+
+  // clear on mouselave even if mousemove didn't catch it.
+  SVG.addEventListener('mouseleave', e => {
+    SVG.classList.remove('state--highlight');
+    const mh = SVG.querySelector('.metric--highlight');
+    mh && mh.classList.remove('metric--highlight');
+  });
+
+  // On the first run, tease with a little peek reveal
+  async function peekGauge(SVG) {
+    // Delay just a tad to let the user aclimatize beforehand.
+    await delay(1000);
+
+    // To visually get the outer ring to peek on the edge, we need the inner ring on top. This is SVG's equivalent to `innerElem.zIndex = 100`
+    const inner = SVG.querySelector('.lh-gauge__inner');
+    let id = `uniq-${Math.random()}`;
+    inner.setAttribute('id', id);
+    const useElem = document.createElementNS(NS_URI, 'use');
+    useElem.setAttribute('href', `#${id}`);
+    // for paint order this must come _after_ the outer.
+    SVG.appendChild(useElem);
+
+    const peekDurationSec = 2.5;
+    SVG.style.setProperty('--peek-dur', `${peekDurationSec}s`);
+    SVG.classList.add('state--peek', 'state--expanded');
+
+    // Fancy double cleanup
+    const cleanup = _ => SVG.classList.remove('state--peek', 'state--expanded') || useElem.remove();
+    const tId = setTimeout(_ => {
+      SVG.removeEventListener('mouseenter', handleEarlyInteraction);
+      cleanup();
+    }, peekDurationSec * 1000 * 1.5); // lil extra time just cuz
+    function handleEarlyInteraction() {
+      clearTimeout(tId);
+      cleanup();
+    }
+    SVG.addEventListener('mouseenter', handleEarlyInteraction, {once: true});
+  }
+}
---
+++ script/main.js
@@ -0,0 +1,358 @@
+import { h, render, createRef, Component } from 'preact';
+import { QUANTILE_AT_VALUE, VALUE_AT_QUANTILE } from './math.js';
+import { $, NBSP, numberFormatter, calculateRating, arithmeticMean } from './util.js';
+import { metrics, scoringGuides } from './metrics.js';
+import { updateGauge } from './gauge.js';
+
+const params = new URLSearchParams(location.hash.substr(1));
+
+function determineMinMax(metricScoring) {
+  const valueAtScore100 = VALUE_AT_QUANTILE(metricScoring, 0.995);
+  const valueAtScore5 = VALUE_AT_QUANTILE(metricScoring, 0.05);
+
+  let min = Math.floor(valueAtScore100 / 1000) * 1000;
+  let max = Math.ceil(valueAtScore5 / 1000) * 1000;
+  let step = 10;
+
+  // Special handling for CLS
+  if (metricScoring.units === 'unitless') {
+    min = 0;
+    max = Math.ceil(valueAtScore5 * 100) / 100;
+    step = 0.01;
+  }
+
+  return {
+    min,
+    max,
+    step,
+  };
+}
+
+/**
+ * @param {string} version
+ */
+function getMajorVersion(version) {
+  return version.split('.')[0];
+}
+
+class Metric extends Component {
+  onValueChange(e) {
+    const {id} = this.props;
+
+    this.props.app.setState({
+      metricValues: {
+        ...this.props.app.state.metricValues,
+        [id]: e.target.valueAsNumber,
+      },
+    });
+  }
+
+  onScoreChange(e) {
+    const {id, metricScoring} = this.props;
+
+    const score = e.target.valueAsNumber;
+    let computedValue = VALUE_AT_QUANTILE(metricScoring, score / 100);
+
+    // Clamp because we can end up with Infinity
+    const { min, max } = determineMinMax(metricScoring);
+    computedValue = Math.max(Math.min(computedValue, max), min);
+
+    if (metricScoring.units !== 'unitless') {
+      computedValue = Math.round(computedValue);
+    }
+
+    this.props.app.setState({
+      metricValues: {
+        ...this.props.app.state.metricValues,
+        [id]: computedValue,
+      },
+    });
+  }
+
+  render({ id, value, score, weightMax, metricScoring }) {
+    const { min, max, step } = determineMinMax(metricScoring, id);
+    const weight = metricScoring.weight;
+    const valueFormatted = metricScoring.units === 'unitless' ?
+      value.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2}) :
+      // TODO: Use https://github.com/tc39/proposal-unified-intl-numberformat#i-units when Safari/FF support it
+      `${numberFormatter.format(value)}${NBSP}ms`;
+    const weightFormatted = (weight * 100).toLocaleString(undefined, {maximumFractionDigits: 1});
+
+    return <tr class={`lh-metric--${calculateRating(score / 100)}`}>
+      <td>
+        <span class="lh-metric__score-icon"></span>
+      </td>
+      <td>{`${id} (${metricScoring.name})`}</td>
+      <td>
+        <input type="range" min={min} value={value} max={max} step={step} class={`${id} metric-value`} onInput={(e) => this.onValueChange(e)} />
+        <output class="${id} value-output">{valueFormatted}</output>
+      </td>
+      <td></td>
+
+      <td>
+        <input type="range" class={`${id} metric-score`} style={`width: ${weight / weightMax * 100}%`} value={score} onInput={(e) => this.onScoreChange(e)} />
+        <output class={`${id} score-output`}>{score}</output>
+      </td>
+
+      <td>
+        <span class={`${id} weight-text`}>{weightFormatted}%</span>
+      </td>
+    </tr>
+  }
+}
+
+class Gauge extends Component {
+  constructor(props) {
+    super(props);
+    this.ref = createRef();
+  }
+
+  refreshGauge() {
+    updateGauge(this.ref.current, {
+      title: 'Performance',
+      auditRefs: this.props.auditRefs,
+      id: 'performance',
+      score: this.props.score,
+    });
+  }
+
+  componentDidMount() {
+    this.refreshGauge();
+  }
+
+  componentDidUpdate() {
+    this.refreshGauge();
+  }
+
+  render({ score }) {
+    return (
+      <div ref={this.ref} class={`lh-gauge__wrapper lh-gauge__wrapper--${calculateRating(score)}`}>
+        <div class='lh-gauge__svg-wrapper'>
+          <svg class='lh-gauge state--expanded'>
+            <g class='lh-gauge__inner'>
+              <circle class='lh-gauge__bg' />
+              <circle class='lh-gauge__base lh-gauge--faded' />
+              <circle class='lh-gauge__arc' />
+              <text class='lh-gauge__percentage'></text>
+            </g>
+            <g class='lh-gauge__outer'>
+              <circle class='cover' />
+            </g>
+          </svg>
+        </div>
+      </div>
+    );
+  }
+}
+
+class ScoringGuide extends Component {
+  render({ app, name, values, scoring }) {
+    // Make sure weights total to 1
+    const weights = Object.values(scoring).map(metricScoring => metricScoring.weight);
+    const weightSum = weights.reduce((agg, val) => (agg += val));
+    const weightMax = Math.max(...Object.values(weights));
+    console.assert(weightSum > 0.999 && weightSum < 1.0001); // lol rounding is hard.
+
+    const metricsData = Object.keys(scoring).map(id => {
+      const metricScoring = scoring[id];
+
+      const result = {
+        id,
+        metricScoring,
+        value: values[id],
+        score: Math.round(QUANTILE_AT_VALUE(metricScoring, values[id]) * 100),
+      };
+
+      return result;
+    });
+
+    const auditRefs = metricsData.map(metric => {
+      return {
+        id: metric.id,
+        weight: metric.metricScoring.weight,
+        group: 'metrics',
+        result: {
+          score: metric.score / 100,
+        },
+      };
+    });
+
+    const score = arithmeticMean(auditRefs);
+
+    let title = <h2>{name}</h2>;
+    if (name === 'v10') {
+      title = <h2>latest<br/><i><a href="https://github.com/GoogleChrome/lighthouse/releases/tag/v10.0.0">v10</a></i></h2>;
+    } else if (name === 'v8') {
+      title = <h2><i><a href="https://github.com/GoogleChrome/lighthouse/releases/tag/v8.0.0">v8, v9</a></i></h2>;
+    } else if (name === 'v6') {
+      title = <h2><i><a href="https://github.com/GoogleChrome/lighthouse/releases/tag/v6.0.0">v6, v7</a></i></h2>;
+    }
+
+    return <form class="wrapper">
+      {title}
+
+      <table>
+        <thead>
+          <tr>
+            <th class="th--metric" colspan="2"></th>
+            <th class="th--value">Value</th>
+            <th class="th--spacer"></th>
+            <th class="th--score">Metric Score</th>
+            <th class="th--weight">Weighting</th>
+          </tr>
+        </thead>
+        <tbody>
+          {metricsData.map(metric => {
+            return <Metric app={app} weightMax={weightMax} metricScoring={metric.metricScoring} {...metric}></Metric>
+          })}
+        </tbody>
+      </table>
+
+      <div class="perfscore">
+        <Gauge score={score} auditRefs={auditRefs}></Gauge>
+      </div>
+    </form>
+  }
+}
+
+const debounce = (callback, time = 250, interval) =>
+  ((...args) => {
+    clearTimeout(interval);
+    interval = setTimeout(() => callback(...args), time);
+  });
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = getInitialState();
+    this.onDeviceChange = this.onDeviceChange.bind(this);
+    this.onVersionsChange = this.onVersionsChange.bind(this);
+    // debounce to avoid flooding with new URLs
+    this.debouncedUpdatePermalink = debounce(this.updatePermalink);
+  }
+
+  updatePermalink(state) {
+    const {versions, device, metricValues} = state;
+    const url = new URL(location.href);
+    const auditIdValuePairs = Object.entries(metricValues).map(([id, value]) => {
+      return [id, value];
+    });
+    const params = new URLSearchParams(auditIdValuePairs);
+    params.set('device', device);
+    for (const version of versions) params.append('version', version);
+    url.hash = params.toString();
+    history.replaceState(state, '', url.toString());
+  }
+
+  componentDidUpdate() {
+    this.debouncedUpdatePermalink(this.state);
+  }
+
+  onDeviceChange(e) {
+    this.setState({device: e.target.value});
+  }
+
+  onVersionsChange(e) {
+    this.setState({versions: e.target.value.split(',')});
+  }
+
+  normalizeVersions(versions) {
+    return versions.map(version => {
+      version = parseInt(version, 10);
+      if (version < 5) {
+        throw new Error(`Unsupported Lighthouse version (${version})`);
+      }
+      
+      switch (version) {
+        default:
+        case 12:
+        case 11:
+        case 10:
+          return 10;
+        case 9:
+        case 8:
+          return 8;
+        case 7:
+        case 6:
+          return 6;
+      }
+    });
+  }
+
+  render() {
+    const {versions, device, metricValues} = this.state;
+
+    const normalizedVersions = this.normalizeVersions(versions);
+
+    const scoringGuideEls = normalizedVersions.map(version => {
+      const key = `v${version}`;
+      console.assert(scoringGuides[key], `scoringGuide for ${key} doesnt exist`)
+      return <ScoringGuide app={this} name={key} values={metricValues} scoring={scoringGuides[key][device]}></ScoringGuide>;
+    });
+    return <div class="app">
+      <div class="controls wrapper">
+        <label>Device type:
+          <select name="device" value={device} onChange={this.onDeviceChange} >
+            <option value="mobile">Mobile</option>
+            <option value="desktop">Desktop</option>
+          </select>
+        </label>
+        <label>Versions:
+          <select name="versions" value={normalizedVersions.join(',')} onChange={this.onVersionsChange} >
+            <option value="10,8,6,5">show all</option>
+            <option value="10">v10, v11, v12</option>
+            <option value="8">v8, v9</option>
+            <option value="6">v6, v7</option>
+            <option value="5">v5</option>
+          </select>
+        </label>
+      </div>
+      {scoringGuideEls}
+    </div>
+  }
+}
+
+function getInitialState() {
+  const availableScoringGuides = Object.keys(scoringGuides).map(k => parseInt(k.replace('v',''), 10)).sort((a, b) => b - a);
+
+  const versions = params.has('version') ?
+    params.getAll('version').map(getMajorVersion) :
+    [`${availableScoringGuides.at(0) || 8}`]; // version (or versions) to show by default
+
+  // Default to mobile if it's not matching our known emulatedFormFactors. https://github.com/GoogleChrome/lighthouse/blob/master/types/externs.d.ts#:~:text=emulatedFormFactor
+  let device = params.get('device');
+  if (device && device !== 'mobile' && device !== 'desktop') {
+    console.warn(`Invalid emulatedFormFactors value: ${device}. Fallback to mobile scoring.`);
+    device = 'mobile';
+  } else if (!device) {
+    // Device not expressed in the params
+    device = 'mobile';
+  }
+
+  const metricValues = {};
+  // If no metric values come in w/ params, initalize with mobile medians (score of 50)
+  const metricScorings = {...scoringGuides.v5.mobile, ...scoringGuides.v8.mobile}; // v5 is neccessary for FCI
+  for (const id in metricScorings) {
+    metricValues[id] = metricScorings[id].median;
+  }
+
+  // Populate metricValues from query string.
+  for (const [id, metric] of Object.entries(metrics)) {
+    const value = params.get(id) || params.get(metric.auditId);
+    if (!value) continue;
+    metricValues[id] = Number(value);
+  }
+
+  return {
+    versions,
+    device,
+    metricValues,
+  };;
+}
+
+function main() {
+  render(<App></App>, $('#container'));
+}
+
+// just one call to main because i'm basic like that
+main();
